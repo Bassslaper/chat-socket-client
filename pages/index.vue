93eedd501fa0c1f99b03e319e88c113ef16d7311
Alexandr Abrosimov <alex.a@mactelabs.com>
2021-08-30 23:11:46 +0700

    Initialized an empty projectr, set up a connection to socket io server

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-btn
+        @click="sendMessage"
+      >
+        NEW MESSAGE
+      </v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  sockets: {
+    connect () {
+      console.log('Conection on client is Done')
+    }
+  },
+  methods: {
+    sendMessage () {
+      this.$socket.emit('createMessage', {
+        text: 'FROM CLIENT'
+      })
+    }
+  }
+
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
