c2d2bd6290ba2b4c6d063692263b4a81eb633156
Alexandr Abrosimov <alex.a@mactelabs.com>
2021-08-31 21:51:26 +0700

    Added Login into room component

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,42 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-btn
-        @click="sendMessage"
-      >
-        NEW MESSAGE
-      </v-btn>
+    <v-flex xs12 sm8>
+      <v-card min-width="400">
+        <v-card-title>
+          NUXT CHAT
+        </v-card-title>
+        <v-card-text>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+          >
+            <v-text-field
+              v-model="name"
+              :counter="16"
+              :rules="nameRules"
+              label="Ваше имя"
+              required
+            />
+
+            <v-text-field
+              v-model="room"
+              :rules="roomRules"
+              label="Введите комнату"
+              required
+            />
+
+            <v-btn
+              :disabled="!valid"
+              color="primary"
+              class="mr-4"
+              @click="submit"
+            >
+              Войти
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
@@ -13,17 +44,32 @@
 <script>
 
 export default {
+  layout: 'empty',
   sockets: {
     connect () {
       console.log('Conection on client is Done')
     }
   },
+  data: () => ({
+    valid: true,
+    name: '',
+    nameRules: [
+      v => !!v || 'Введите имя',
+      v => (v && v.length <= 16) || 'Имя не должно превышать 16 символов'
+    ],
+    room: '',
+    roomRules: [
+      v => !!v || 'Введите комнату'
+    ]
+  }),
+
   methods: {
-    sendMessage () {
-      this.$socket.emit('createMessage', {
-        text: 'FROM CLIENT'
-      })
+    submit () {
+      if (this.$refs.form.validate()) {
+        console.log('Добро пожаловать')
+      }
     }
+
   }
 
 }
